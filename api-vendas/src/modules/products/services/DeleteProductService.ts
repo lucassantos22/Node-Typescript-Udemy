044cbd1a647fc765d0d93b6bd467fa5b91a76730
Lucas Santos <lufigueredo@uolinc.com>
2022-06-01 19:16:21 -0300

    chore: add DeleteProductService

--- api-vendas/src/modules/products/services/DeleteProductService.ts
+++ api-vendas/src/modules/products/services/DeleteProductService.ts
@@ -1,20 +1,19 @@
 import AppError from "@shared/errors/AppError";
 import { getCustomRepository } from "typeorm";
 
-import Product from "../typeorm/entities/Product";
 import { ProductRepository } from "../typeorm/repositories/ProductsRepository";
 
 interface IRequest {
   id: string
 }
 
-export default class ShowProductService {
-  public async execute ({id}: IRequest): Promise<Product | undefined> {
+export default class DeleteProductService {
+  public async execute ({id}: IRequest): Promise<void> {
     const productsRepository = getCustomRepository(ProductRepository)
     const product = await productsRepository.findOne(id)
     if (!product) {
       throw new AppError('Product not found.')
     }
-    return product;
+    productsRepository.remove(product)
   }
 }
