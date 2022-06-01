fb311a4d82367d90d5594cd7c3cdbe413100a2dc
Lucas Santos <lufigueredo@uolinc.com>
2022-05-31 21:39:46 -0300

    chore: add ShowProductService

---
+++ api-vendas/src/modules/products/services/ShowProductService.ts
@@ -0,0 +1,20 @@
+import AppError from "@shared/errors/AppError";
+import { getCustomRepository } from "typeorm";
+
+import Product from "../typeorm/entities/Product";
+import { ProductRepository } from "../typeorm/repositories/ProductsRepository";
+
+interface IRequest {
+  id: string
+}
+
+export default class ShowProductService {
+  public execute ({id}: IRequest): Promise<Product | undefined> {
+    const productsRepository = getCustomRepository(ProductRepository)
+    const product = productsRepository.findOne(id)
+    if (!product) {
+      throw new AppError('Product not found.')
+    }
+    return product;
+  }
+}
