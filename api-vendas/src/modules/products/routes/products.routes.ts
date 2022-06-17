4990bf0b75d93a82cbf8fbdf59591fcc95ffd3db
Lucas Santos <lufigueredo@uolinc.com>
2022-06-17 00:20:08 -0300

    chore: add celebrate validation

--- api-vendas/src/modules/products/routes/products.routes.ts
+++ api-vendas/src/modules/products/routes/products.routes.ts
@@ -1,13 +1,55 @@
 import { Router } from 'express'
+import { celebrate, Joi, Segments } from 'celebrate'
+
 import ProductsController from '../controllers/ProductsController'
 
 const productRoutes = Router();
 const productsController = new ProductsController()
 
 productRoutes.get('/', productsController.index)
-productRoutes.get('/:id', productsController.show)
-productRoutes.post('/', productsController.create)
-productRoutes.put('/:id', productsController.update)
-productRoutes.delete('/:id', productsController.delete)
+
+productRoutes.get(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required
+    }
+  }),
+  productsController.show
+)
+
+productRoutes.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      price: Joi.number().precision(2).required(),
+      quantity: Joi.number().required()
+    }
+  }),
+  productsController.create
+)
+
+productRoutes.put(
+  '/:id',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      price: Joi.number().precision(2).required(),
+      quantity: Joi.number().required()
+    }
+  }),
+  productsController.update
+)
+
+productRoutes.delete(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required
+    }
+  }),
+  productsController.delete
+)
 
 export default productRoutes
