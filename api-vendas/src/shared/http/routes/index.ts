9bef3e9e6e54fd3a6168efeeaf6c3814d5f4d50c
Lucas Santos <lufigueredo@uolinc.com>
2022-06-23 21:46:13 -0300

    chore: add sessions router

--- api-vendas/src/shared/http/routes/index.ts
+++ api-vendas/src/shared/http/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express'
 import productRoutes from '@modules/products/routes/products.routes';
 import usersRoutes from '@modules/users/routes/users.routes';
+import sessionsRoutes from '@modules/users/routes/users.routes';
 
 const routes = Router();
 
 routes.use('/products', productRoutes)
 routes.use('/users', usersRoutes)
+routes.use('/sessions', sessionsRoutes)
 
 export default routes
