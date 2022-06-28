6522dd343c7059c464fbe8dfa71adcc3f8311d5a
Lucas Santos <lufigueredo@uolinc.com>
2022-06-28 15:45:23 -0300

    chore: add jsonwebtoken

--- api-vendas/src/modules/users/services/CreateSessionsService.ts
+++ api-vendas/src/modules/users/services/CreateSessionsService.ts
@@ -1,17 +1,23 @@
 import { getCustomRepository } from "typeorm";
+import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
 
 import AppError from "@shared/errors/AppError";
 import User from "../typeorm/entities/User";
 import UsersRepository from "../typeorm/repositories/UserRepository";
-import { compare } from "bcryptjs";
 
 interface IRequest {
   email: string;
   password: string;
 }
 
+interface IResponse {
+  user: User;
+  token: string;
+}
+
 export default class CreateSessionsService {
-  public async execute ({ email, password }: IRequest): Promise<User> {
+  public async execute ({ email, password }: IRequest): Promise<IResponse> {
     const usersRepository = getCustomRepository(UsersRepository)
     const user = await usersRepository.findByEmail(email)
 
@@ -25,6 +31,18 @@ export default class CreateSessionsService {
       throw new AppError('Incorrect email/password combination.', 401)
     }
 
-    return user
+    const token = sign(
+      {},
+      '5f40c23e852fa561d9923baa05990976',
+      {
+        subject: user.id,
+        expiresIn: '1d'
+      }
+    )
+
+    return {
+      user,
+      token
+    }
   }
 }
