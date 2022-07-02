d24385b48830cabf64ae7a1c4277b4d93502930c
Lucas Santos <lufigueredo@uolinc.com>
2022-07-02 17:49:56 -0300

    chore: override obj request

--- api-vendas/src/shared/http/middlewares/isAuthenticated.ts
+++ api-vendas/src/shared/http/middlewares/isAuthenticated.ts
@@ -1,8 +1,14 @@
-import { Request, Response, NextFunction } from 'express';
+import { Request, Response, NextFunction, request } from 'express';
 import { verify } from 'jsonwebtoken';
 
 import AppError from '@shared/errors/AppError';
 import authConfig from '@config/auth';
+
+interface TokenPayload {
+  iat: number;
+  exp: number;
+  sub: string;
+}
 
 export default function isAuthenticated(req: Request, res: Response, next: NextFunction): void {
   const authHeader = req.headers.authorization
@@ -16,6 +22,10 @@ export default function isAuthenticated(req: Request, res: Response, next: NextF
 
   try {
     const decodedToken = verify(token, authConfig.jwt.secret)
+    const { sub } = decodedToken as TokenPayload
+    req.user = {
+      id: sub
+    }
     next()
   } catch {
     throw new AppError('Invalid JWT Token.')
