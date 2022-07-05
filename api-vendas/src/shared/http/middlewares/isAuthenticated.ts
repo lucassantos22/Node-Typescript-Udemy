05c180ee770114fd4a2328b3463d02d7ebb7d739
Lucas Santos <lufigueredo@uolinc.com>
2022-07-05 17:12:34 -0300

    chore: add multer

--- api-vendas/src/shared/http/middlewares/isAuthenticated.ts
+++ api-vendas/src/shared/http/middlewares/isAuthenticated.ts
@@ -4,7 +4,7 @@ import { verify } from 'jsonwebtoken';
 import AppError from '@shared/errors/AppError';
 import authConfig from '@config/auth';
 
-interface TokenPayload {
+interface ITokenPayload {
   iat: number;
   exp: number;
   sub: string;
@@ -22,7 +22,7 @@ export default function isAuthenticated(req: Request, res: Response, next: NextF
 
   try {
     const decodedToken = verify(token, authConfig.jwt.secret)
-    const { sub } = decodedToken as TokenPayload
+    const { sub } = decodedToken as ITokenPayload
     req.user = {
       id: sub
     }
